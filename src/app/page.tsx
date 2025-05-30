4465fb5d935af1384fe45249ac55d251506b5db0
Nielleeeee <jandnllplz@gmail.com>
2025-05-31 01:27:57 +0800

    chore: minor UI changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import MessageForm from "@/components/form/form";
-import MessageResponse from "@/components/messageResponse/messageResponse";
+import PlaceList from "@/components/placeList/placeList";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2">
-      <section className="container w-full px-4 mx-auto">
-        <h1>Restaurant Finder</h1>
+      <section className="container w-full py-8 px-4 mx-auto">
+        <h1 className="text-3xl font-bold mb-10 text-center">Place Finder</h1>
+
+        <PlaceList />
 
         <MessageForm />
-
-        <MessageResponse />
       </section>
     </main>
   );
